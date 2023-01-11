5f36dfb93a3900d8e09bf11561f66a5ef69241bc
SimonLukin <99149000+SimonLukin@users.noreply.github.com>
2023-01-11 13:34:25 +0200

    Add files via upload

---
+++ Ex04.Menus. Interfaces/Menu.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MainMenu
+    {
+        #region Members
+        private List<MainMenu> m_Menus;
+        private List<MenuItem> m_MenuItems;
+        private String m_MenuTitle;
+        byte m_MenuIndex;
+        #endregion
+
+        #region Properties
+        public List<MainMenu> Menus
+        {
+            get => m_Menus;
+        }
+
+        public List<MenuItem> MenuItems
+        {
+            get => m_MenuItems;
+        }
+
+        public String MenuTitle
+        {
+            get => m_MenuTitle;
+            set => m_MenuTitle = value;
+        }
+        #endregion
+
+        public MainMenu()
+        {
+
+        }
+
+        #region Methods
+        public void GoToPreviousMenu()
+        {
+
+        }
+
+        public void ExitMenu()
+        {
+            Environment.Exit(0);
+        }
+
+        public MenuItem SelectItem(byte optionNumber)
+        {
+            return m_MenuItems[optionNumber - 1];
+        } 
+
+        public void AddMenu(MainMenu menu)
+        {
+            m_Menus.Add(menu);
+        }
+
+        public void AddMenuItem(MenuItem item)
+        {
+            m_MenuItems.Add(item);
+        }
+
+        public void RemoveMenuItem(MenuItem item)
+        {
+            m_MenuItems.Remove(item);
+        }
+        #endregion
+    }
+}
---
+++ Ex04.Menus. Interfaces/MenuItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class MenuItem
+    {
+        private string m_Name;
+        private byte m_Index;
+
+        public string Name
+        {
+            get => m_Name;
+            set => m_Name = value;
+        }
+
+        public byte Index
+        {
+            get => m_Index;
+            set=> m_Index = value;
+        }
+    }
+}
